c47814a317805f0d0ed84d0e0f57729220136198
GabiAle97 <43727640+GabiAle97@users.noreply.github.com>
2020-03-08 13:21:50 -0300

    Add files via upload

--- Test para Ingresantes - NCR/Algoritmos/Buscando anagramas/script.js
+++ Test para Ingresantes - NCR/Algoritmos/Buscando anagramas/script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         var message;
         var isClass;
         var total = solution(anagrama,subcadena);
+        //Verifica que los ingresos no estén vacios
         if (anagrama != '' && subcadena != '') {
             message=`La subcadena "${subcadena}" aparece ${total} veces en el texto`
             isClass = 'alert alert-success'
@@ -13,6 +14,7 @@ $(document).ready(function(){
             message=`Ninguno de los campos pueden estar vacios`
             isClass = 'alert alert-danger'
         }
+        //Envía el mensaje de Resultado
         $("#result").empty()
         var alert = $("<div>",{
             'class': isClass,
@@ -24,7 +26,7 @@ $(document).ready(function(){
         
     });
 })
-
+//Funcion que se encarga de separar por letra los strings obtenidos y contar la cantidad de anagramas
 function solution(A,B) {
     A= A.toLowerCase().split("")
     B= B.toLowerCase().split("")
@@ -32,6 +34,7 @@ function solution(A,B) {
     var max = A.length - length + 1;
     var counter = 0;
     for (let i = 0; i < max; i++) {
+        //Checkea por cada entrada de B si el fragmento de A obtenido concuerda consigo mismo. En dicho caso, aumenta la variable "counter"
         if (check(B,A.slice(i,(i+length)))){
             counter++
         }
@@ -39,11 +42,10 @@ function solution(A,B) {
     return counter;
 }
 
+//Funcion que checkea que ambos strings obtenidos de "solution" sean iguales
 function check(oldString, newString ) {
     var A= oldString.sort().join("");
     var B= newString.sort().join("");
-    console.log(A)
-    console.log(B)
     if (A==B) {
         return true;
     }else{
